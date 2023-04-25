9fbdc97394f12d8eb2782d208158dabc5d556568
Kit La Touche <kit@transneptune.net>
2023-04-24 20:54:15 -0700

    Major wtf

--- scripts/r-maps.js
+++ scripts/r-maps.js
@@ -39,37 +39,6 @@ class RMaps {
   }
 }
 
-class RMapToolsLayer extends InteractionLayer {
-	constructor() {
-		super();
-		console.log('R-Maps | Loaded into canvas');
-	}
-
-	activate() {
-		super.activate();
-		console.log('R-Maps | Activated');
-		return this;
-	}
-
-	deactivate() {
-		super.deactivate();
-		console.log('R-Maps | Deactivated');
-		return this;
-	}
-
-	async draw() {
-		await super.draw();
-		console.log('R-Maps | Drawing');
-		return this;
-	}
-
-	async tearDown() {
-		await super.tearDown();
-		console.log('R-Maps | Tearing down!');
-		return this;
-	}
-}
-
 class RMapEdgeData {
   static get allEdges() {
     const allEdges = (canvas?.scene.tokens || []).reduce((accumulator, token) => {
@@ -86,20 +55,6 @@ class RMapEdgeData {
 
   static getEdgesForToken(tokenId) {
     return canvas?.scene.tokens.get(tokenId)?.getFlag(RMaps.ID, RMaps.FLAGS.EDGES) || {};
-  }
-
-  static getInboundEdgesForToken(tokenId) {
-    const inboundEdges = Object.keys(this.allEdges).filter(
-      (key) => this.allEdges[key].to === tokenId
-    );
-    return inboundEdges.reduce((accumulator, edgeId) => {
-      const tokenEdge = this.allEdges[edgeId];
-
-      return {
-        ...accumulator,
-        [edgeId]: tokenEdge,
-      };
-    }, {});
   }
 
   static async createEdge(tokenId, edgeData) {
@@ -128,11 +83,13 @@ class RMapEdgeData {
   }
 
   static updateEdge(edgeId, updateData) {
+    /* * /
     const relevantEdge = this.allEdges[edgeId];
     const update = {
       [edgeId]: updateData
     };
     return canvas?.scene.tokens.get(relevantEdge.fromId)?.setFlag(RMaps.ID, RMaps.FLAGS.EDGES, update);
+    /* */
   }
 
   static deleteEdge(edgeId) {
@@ -146,99 +103,60 @@ class RMapEdgeData {
       .get(relevantEdge.fromId)?.setFlag(RMaps.ID, RMaps.FLAGS.EDGES, keyDeletion);
   }
 
-  // TODO: this doesn't need to clear and redraw; we can redraw in-situ and
-  // save any styling.
-  static clearEdgeDrawingsForToken(tokenId) {
-    const inboundEdges = Object.keys(this.allEdges).filter(
-      (key) => this.allEdges[key].to === tokenId
-    );
-    const outboundEdges = Object.keys(this.getEdgesForToken(tokenId));
-    [...outboundEdges, ...inboundEdges].forEach(async (edgeId) => {
-      const edge = this.allEdges[edgeId];
-      if (edge.drawing) {
-        canvas?.drawings.get(edge.drawing).document.delete();
-        await this.updateEdge(edgeId, { drawing: null });
-      }
+  // This pertains to Drawings:
+  static async updateEdgeDrawingsForToken(tokenId) {
+    /* Temporarily commented out. * /
+    // Inbound edges:
+    const inbound = Object.values(this.allEdges).filter(
+      (edge) => edge.to === tokenId
+    ).map((edge) => {
+      const { drawingId } = edge;
+      const drawing = canvas.scene.drawings.get(drawingId);
+
+      const fromNode = canvas.scene.tokens.get(edge.fromId).object.center;
+      const toNode = canvas.scene.tokens.get(edge.to).object.center;
+
+      const newEdge = getEdgeGivenTwoNodes(fromNode, toNode);
+      return {
+        _id: drawingId,
+        ...newEdge,
+      };
     });
+    // Outbound edges:
+    const outbound = Object.values(
+      this.getEdgesForToken(tokenId)
+    ).map((edge) => {
+      const { drawingId } = edge;
+      const drawing = canvas.scene.drawings.get(drawingId);
+
+      const fromNode = canvas.scene.tokens.get(tokenId).object.center;
+      const toNode = canvas.scene.tokens.get(edge.to).object.center;
+
+      const newEdge = getEdgeGivenTwoNodes(fromNode, toNode);
+      return {
+        _id: drawingId,
+        ...newEdge,
+      };
+    });
+    const updates = await canvas.scene.updateEmbeddedDocuments('Drawing', [...inbound, ...outbound])
+    return updates;
+    /* */
   }
 
-  static drawEdgesForToken(tokenId) {
-    const inboundEdges = Object.keys(this.allEdges).filter(
-      (key) => this.allEdges[key].to === tokenId
-    );
-    const outboundEdges = Object.keys(this.getEdgesForToken(tokenId));
-    [...outboundEdges, ...inboundEdges].forEach((edgeId) => {
-      this.drawEdge(edgeId);
-    });
-  }
-
+  // This pertains to Drawings:
   static async drawEdge(edgeId) {
+    /* Temporarily commented out. * /
     const relevantEdge = this.allEdges[edgeId];
     const fromNode = canvas?.scene.tokens.get(relevantEdge.fromId)._object.center;
     const toNode = canvas?.scene.tokens.get(relevantEdge.to)._object.center;
 
-    // This is the most BRUTE FORCE way I could see to guarantee that the
-    // bounding box and corners and lines all match up correctly. It works. It
-    // could be improved.
-    //
-    // Calculate corners:
-    const UL = { x: 0, y: 0 };
-    const UR = { x: Math.abs(fromNode.x - toNode.x), y: 0 }
-    const LL = { x: 0, y: Math.abs(fromNode.y - toNode.y) }
-    const LR = { x: Math.abs(fromNode.x - toNode.x), y: Math.abs(fromNode.y - toNode.y) }
-
-    // Find the corner we're starting in. We are therefore moving to the
-    // opposite corner.
-    let origin, destination;
-    if (
-      // UL:
-      fromNode.x == Math.min(fromNode.x, toNode.x)
-      && fromNode.y == Math.min(fromNode.y, toNode.y)
-    ) {
-      origin = UL;
-      destination = LR;
-    } else if (
-      // UR:
-      fromNode.x == Math.max(fromNode.x, toNode.x)
-      && fromNode.y == Math.min(fromNode.y, toNode.y)
-    ) {
-      origin = UR
-      destination = LL;
-    } else if (
-      // LL:
-      fromNode.x == Math.min(fromNode.x, toNode.x)
-      && fromNode.y == Math.max(fromNode.y, toNode.y)
-    ) {
-      origin = LL;
-      destination = UR;
-    } else if (
-      // LR:
-      fromNode.x == Math.max(fromNode.x, toNode.x)
-      && fromNode.y == Math.max(fromNode.y, toNode.y)
-    ) {
-      origin = LR;
-      destination = UL;
-    }
-
-    // Now just prep the Drawing object:
-    const edge = {
-      x: Math.min(fromNode.x, toNode.x),
-      y: Math.min(fromNode.y, toNode.y),
-      shape: {
-        width: Math.abs(toNode.x - fromNode.x),
-        height: Math.abs(toNode.y - fromNode.y),
-        type: foundry.data.ShapeData.TYPES.POLYGON,
-        points: [
-          origin.x, origin.y,
-          destination.x, destination.y,
-        ],
-      },
-      // TODO: colour and style
-    };
+    const edge = getEdgeGivenTwoNodes(fromNode, toNode);
+    edge.shape.type = foundry.data.ShapeData.TYPES.POLYGON;
 
     const [ drawing ] = await canvas.scene.createEmbeddedDocuments('Drawing', [edge]);
-    this.updateEdge(edgeId, { drawing: drawing._id });
+    this.updateEdge(edgeId, { drawingId: drawing._id });
     return drawing;
+    /* */
   }
 }
 
@@ -286,6 +204,65 @@ class Line extends PIXI.Graphics {
   }
 }
 
+function getEdgeGivenTwoNodes(fromNode, toNode) {
+  // This is the most BRUTE FORCE way I could see to guarantee that the
+  // bounding box and corners and lines all match up correctly. It works. It
+  // could be improved.
+  //
+  // Calculate corners:
+  const UL = { x: 0, y: 0 };
+  const UR = { x: Math.abs(fromNode.x - toNode.x), y: 0 }
+  const LL = { x: 0, y: Math.abs(fromNode.y - toNode.y) }
+  const LR = { x: Math.abs(fromNode.x - toNode.x), y: Math.abs(fromNode.y - toNode.y) }
+
+  // Find the corner we're starting in. We are therefore moving to the
+  // opposite corner.
+  let origin, destination;
+  if (
+    // UL:
+    fromNode.x == Math.min(fromNode.x, toNode.x)
+    && fromNode.y == Math.min(fromNode.y, toNode.y)
+  ) {
+    origin = UL;
+    destination = LR;
+  } else if (
+    // UR:
+    fromNode.x == Math.max(fromNode.x, toNode.x)
+    && fromNode.y == Math.min(fromNode.y, toNode.y)
+  ) {
+    origin = UR
+    destination = LL;
+  } else if (
+    // LL:
+    fromNode.x == Math.min(fromNode.x, toNode.x)
+    && fromNode.y == Math.max(fromNode.y, toNode.y)
+  ) {
+    origin = LL;
+    destination = UR;
+  } else if (
+    // LR:
+    fromNode.x == Math.max(fromNode.x, toNode.x)
+    && fromNode.y == Math.max(fromNode.y, toNode.y)
+  ) {
+    origin = LR;
+    destination = UL;
+  }
+
+  // Now just prep the Drawing object:
+  return {
+    x: Math.min(fromNode.x, toNode.x),
+    y: Math.min(fromNode.y, toNode.y),
+    shape: {
+      width: Math.abs(toNode.x - fromNode.x),
+      height: Math.abs(toNode.y - fromNode.y),
+      points: [
+        origin.x, origin.y,
+        destination.x, destination.y,
+      ],
+    },
+  };
+}
+
 
 // ============== hooks.js
 // Inject tool into Tokens controls
@@ -293,8 +270,10 @@ Hooks.on('getSceneControlButtons', (buttons) => {
 	RMaps.onGetSceneControlButtons(buttons);
 });
 
-// TODO: add drag-to-draw functionality
 Hooks.on('libWrapper.Ready', () => {
+  // Reset all the wrappers for this module:
+  libWrapper.unregister_all(RMaps.ID);
+
   // TODO: this currently won't fire if you start inside a token?
   libWrapper.register(RMaps.ID, 'TokenLayer.prototype._onDragLeftStart', (wrapped, event) => {
     wrapped(event);
@@ -313,53 +292,64 @@ Hooks.on('libWrapper.Ready', () => {
 
   libWrapper.register(RMaps.ID, 'TokenLayer.prototype._onDragLeftMove', (wrapped, event) => {
     wrapped(event);
-    const spot = xyFromEvent(event);
-    const pixiLine = RMaps.state.pixiLine;
-    pixiLine.update(spot);
+    if (
+      game.activeTool === RMaps.FLAGS.EDGE_TOOL
+      && canvas.tokens.controlledObjects.size === 1
+    ) {
+      const spot = xyFromEvent(event);
+      const pixiLine = RMaps.state.pixiLine;
+      pixiLine.update(spot);
+    }
   }, 'WRAPPER');
 
   // ...Cancel ? How do you even cancel a Drag-and-Drop?
 
   libWrapper.register(RMaps.ID, 'TokenLayer.prototype._onDragLeftDrop', async (wrapped, event) => {
     wrapped(event);
-    // Find if we picked a token:
-    const spot = xyFromEvent(event);
-    const targets = xyInsideTargets(spot);
-    if (targets.length === 1) {
-      // We have a winner.
-      const target = targets[0];
-      const edgeId = await RMapEdgeData.createEdge(RMaps.state.originToken.id, { to: target.id });
-      RMapEdgeData.drawEdge(edgeId);
+    if (
+      game.activeTool === RMaps.FLAGS.EDGE_TOOL
+      && canvas.tokens.controlledObjects.size === 1
+    ) {
+      // Find if we picked a token:
+      const spot = xyFromEvent(event);
+      const targets = xyInsideTargets(spot);
+      if (targets.length === 1) {
+        // We have a winner.
+        const target = targets[0];
+        const edgeId = await RMapEdgeData.createEdge(RMaps.state.originToken.id, { to: target.id });
+        RMapEdgeData.drawEdge(edgeId);
+      }
+      // Clean up:
+      RMaps.state.pixiLine.clear();
+      RMaps.state.pixiLine = null;
     }
-    // Clean up:
-    RMaps.state.pixiLine.clear();
-    RMaps.state.pixiLine = null;
   }, 'WRAPPER');
 
-  libWrapper.register(RMaps.ID, 'Token.prototype._onUpdate', function (wrapped, event, ...args) {
+  // TODO: somehow this is triggering something else that's drawing the edges?
+  libWrapper.register(RMaps.ID, 'Token.prototype._onUpdate', (wrapped, event, ...args) => {
     wrapped(event, ...args);
-
     const keys = Object.keys(foundry.utils.flattenObject(event));
     const changed = new Set(keys);
     const positionChange = ["x", "y"].some(c => changed.has(c));
     const shapeChange = ["width", "height"].some(k => changed.has(k));
     if (positionChange || shapeChange) {
       const { _id } = event;
-      RMapEdgeData.clearEdgeDrawingsForToken(_id);
-      RMapEdgeData.drawEdgesForToken(_id);
+      RMapEdgeData.updateEdgeDrawingsForToken(_id);
     }
   }, 'WRAPPER');
+
+  // TODO: if drawing edge is deleted, remove edge from edge DB flags
 });
 
 // ============== todo.md
-// TODO: add labels on edges
+// These might be handled by Advanced Drawing Tools:
+//     TODO: add labels on edges
+//     TODO: colour and style
 // TODO: support adding intermediate control points and getting bezier-y?
 //          Maybe this is done with the Advanced Drawing Tools mod?
 // TODO: add endcap arrows
 //          Maybe this is done with the Advanced Drawing Tools mod?
 //
+// TODO: permissions stuff is weird?
 // TODO: break this into modular JS
 // TODO: write unit tests. Quench?
-//
-// Since this produces drawings, and players don't always have permissions on
-// drawings, will that be a problem?
